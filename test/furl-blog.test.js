bfa7d22d842a590ade36258735d460b060b63a14
Singh <madhuxsi@3c06303f3ad3.ant.amazon.com>
2022-05-25 11:17:47 -0400

    Initial commit

---
+++ test/furl-blog.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const FurlBlog = require('../lib/furl-blog-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/furl-blog-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new FurlBlog.FurlBlogStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
